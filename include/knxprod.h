46a5e86c85140cb2127eee222485e173ead9b9ef
Franz Reisenhofer <franz.reisenhofer@me.com>
2026-07-21 23:10:32 +0200

    bump knxprod to v1.9

--- include/knxprod.h
+++ include/knxprod.h
@@ -18,7 +18,7 @@
 #define MAIN_FirmwareName "IO-HomeControl KNX Gateway"
 #define MAIN_OpenKnxId 0xAF
 #define MAIN_ApplicationNumber 52
-#define MAIN_ApplicationVersion 24
+#define MAIN_ApplicationVersion 25
 #define MAIN_FirmwareRevision 0
 #define MAIN_ApplicationEncoding iso-8859-15
 #define MAIN_ParameterSize 14285
