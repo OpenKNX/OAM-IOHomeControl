092bed8ee848d875498a6c67c508ea3d1aad2f52
Franz Reisenhofer <franz.reisenhofer@me.com>
2026-09-14 18:39:06 +0200

    chore: integrate automatic 2W device import

--- include/knxprod.h
+++ include/knxprod.h
@@ -18,7 +18,7 @@
 #define MAIN_FirmwareName "IO-HomeControl KNX Gateway"
 #define MAIN_OpenKnxId 0xAF
 #define MAIN_ApplicationNumber 52
-#define MAIN_ApplicationVersion 33
+#define MAIN_ApplicationVersion 34
 #define MAIN_FirmwareRevision 0
 #define MAIN_ApplicationEncoding iso-8859-15
 #define MAIN_ParameterSize 14429
